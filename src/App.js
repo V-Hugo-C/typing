f5ba58f17c5f3813b3bc9727f30b6738334c7b91
Victor Hugo <v-h-c@pop-os.localdomain>
2021-09-15 01:43:01 -0300

    estrutura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (<div className="container" >
+        <div className="validKeys">
+            <span className="matched">Vic</span>
+            <span className="remainder">tor</span>
+        </div>
+        <div className="tipedKeys">1</div>
+        <div className="completedWords">
+            <ol>
+                <li>da</li>
+                <li>daa</li>
+                <li>fas</li>
+            </ol>
+        </div>
+    </div>)
+};
 
 export default App;
